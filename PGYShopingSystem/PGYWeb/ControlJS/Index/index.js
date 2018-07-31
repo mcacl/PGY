de93ad2a679c38d96e2e135885a9d75064d833bd
蒲茂超 <1442169607@qq.com>
2018-07-31 11:25:33 +0800

    添加bootstracp样式

--- PGYShopingSystem/PGYWeb/ControlJS/Index/index.js
+++ PGYShopingSystem/PGYWeb/ControlJS/Index/index.js
@@ -1,3 +1,19 @@
-var Commondata = {title: '蒲公英'};
-new Vue({el: 'div', data: Commondata});
-new Vue({el: 'title', data: Commondata});
+var Commondata = {
+    title: '蒲公英',
+    navs: [
+        {name: '导航1', link: '#'},
+        {name: '关于我们', link: '#'},
+        {name: '导航3', link: '#'}]
+};
+new Vue({el: '#navtitle', data: Commondata});
+new Vue({el: 'a', data: Commondata});
+new Vue({
+    el: 'li', methods: {
+        addActive($event) {
+            $event.currentTarget.className = "active";
+        },
+        removeActive($event) {
+            $event.currentTarget.className = "";
+        }
+    }
+});
