38708884cc4f97b6cadcba351c37c2740de77354
蒲茂超 <1442169607@qq.com>
2018-08-06 18:05:21 +0800

    基础页面

---
+++ PGYShopingSystem/PGYWeb/Common/CommMix.js
@@ -0,0 +1,8 @@
+Vue.mixin({
+    data: function () {
+        return {
+            title: '蒲公英',
+            spimgpath:'SpImage/',
+        };
+    }
+})
--- PGYShopingSystem/PGYWeb/ControlJS/Index/index.js
+++ PGYShopingSystem/PGYWeb/ControlJS/Index/index.js
@@ -1,19 +1,38 @@
-var Commondata = {
-    title: '蒲公英',
-    navs: [
-        {name: '导航1', link: '#'},
-        {name: '关于我们', link: '#'},
-        {name: '导航3', link: '#'}]
-};
-new Vue({el: '#navtitle', data: Commondata});
-new Vue({el: 'a', data: Commondata});
-new Vue({
-    el: 'li', methods: {
-        addActive($event) {
-            $event.currentTarget.className = "active";
+//导航区
+var vuetitle = new Vue({
+    el: '#regtitle',
+    data: {
+        navs: [
+            {name: '导航1', link: '#'},
+            {name: '关于我们', link: '#'},
+            {name: '导航3', link: '#'}]
+    },
+    methods: {
+        addActive: function (event) {
+            event.currentTarget.className = "active";
         },
-        removeActive($event) {
-            $event.currentTarget.className = "";
+        removeActive: function (event) {
+            event.currentTarget.className = "";
         }
     }
+});
+//主窗口区
+var vuemain = new Vue({
+    el: '#regmain',
+    data: {
+        imgs: [
+            {path: 'GuunDoong/gd1.png', link: '#'},
+            {path: 'GuunDoong/gd1.png', link: '#'},
+            {path: 'GuunDoong/gd1.png', link: '#'}
+        ]
+    }
+    // computed: {
+    //     spimgp: function () {
+    //         return this.$data.spimgpath;
+    //     }
+    // }
+});
+//页脚区
+var vuefoot = new Vue({
+    el: '#regfoot'
 });
