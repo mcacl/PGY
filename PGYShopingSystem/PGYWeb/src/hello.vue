4cd3ae7c7d54c5810b64d6ef0790e8baed39201d
蒲茂超 <1442169607@qq.com>
2019-03-05 16:27:07 +0800

    vue换路径

---
+++ PGYShopingSystem/PGYWeb/src/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div >
+    <p>hellow vue</p>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "hello",
+    data() {
+      return {msg: '组件666'};
+    }
+  }
+</script>
---
+++ PGYShopingSystem/PGYWeb/src/index.js
@@ -0,0 +1,7 @@
+import hello from './hello'
+
+new Vue({
+  el: '#app',
+  template: '<hello/>',
+  components: {hello}
+});
